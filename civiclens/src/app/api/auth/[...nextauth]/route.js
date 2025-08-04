95cbd33f158727c9b7a36a4583f156d514f74c07
Karan Singh Parihar <karansingh9560796@gmail.com>
2025-08-04 10:44:32 +0530

    dry run

--- civiclens/src/app/api/auth/[...nextauth]/route.js
+++ civiclens/src/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { dbConnect } from "@/lib/mongodb";
+import { dbConnect } from "../../../../lib/mongodb";
 import User from "../../../../models/Users";
 
 export const authOptions = {
