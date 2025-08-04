3fb8b3e1d0d86a918ad76a986d82f54001f24e06
Muhammad Shoaib Khan <144353922+msk21shoaib@users.noreply.github.com>
2025-08-04 13:14:48 +0530

    complaints model

---
+++ civiclens/src/models/Complaints.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const ComplaintSchema = new mongoose.Schema({
+  deptName: { type: String, required: true },
+  deptMail: { type: String, required: true },
+  subject: { type: String, required: true },
+  description: { type: String, required: true },
+  location: { type: String },
+  complaintNo: { type: String, required: true, unique: true },
+  image: { type: String },
+  createdAt: { type: Date, default: Date.now },
+});
+
+export default mongoose.models.Complaint || mongoose.model("Complaint", ComplaintSchema);
